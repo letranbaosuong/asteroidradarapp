83e4f2273e2bbfc79a8362a87580c090510d0756
letranbaosuong <>
2023-06-13 03:40:54 +0700

    init app

---
+++ app/src/main/java/com/letranbaosuong/asteroidradarapp/main/MainFragment.kt
@@ -0,0 +1,36 @@
+package com.letranbaosuong.asteroidradarapp.main
+
+import android.os.Bundle
+import android.view.*
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.letranbaosuong.asteroidradarapp.R
+import com.letranbaosuong.asteroidradarapp.databinding.FragmentMainBinding
+
+class MainFragment : Fragment() {
+
+    private val viewModel: MainViewModel by lazy {
+        ViewModelProvider(this)[MainViewModel::class.java]
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View {
+        val binding = FragmentMainBinding.inflate(inflater)
+        binding.lifecycleOwner = this
+
+        binding.viewModel = viewModel
+
+        setHasOptionsMenu(true)
+
+        return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.app_menu, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return true
+    }
+}
---
+++ app/src/main/java/com/letranbaosuong/asteroidradarapp/main/MainViewModel.kt
@@ -0,0 +1,6 @@
+package com.letranbaosuong.asteroidradarapp.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+}
